86da596ae595d60769b634cf25ea0f6a49a8cf11
sumu9897 <sumon.bncd@gmail.com>
2025-02-04 18:37:48 +0600

    Navbar Basic Design Done

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { FaSearch } from 'react-icons/fa';
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link href={"/"}>
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-gray-600">Mohammad</span>
+            <span className="text-yellow-300">Estate</span>
+          </h1>
+        </Link>
+        <form>
+            <input type="text" placeholder="Search..." className="bg-transparent focus:outline-none items-center" />
+            <button> <FaSearch className="text-slate-600"/></button>
+        </form>
+        <ul className="flex gap-4">
+            <Link href={'/'}><li className="hidden md:inline text-slate-700 hover:underline">Home</li></Link>
+            <Link href={'/about'}><li className="hidden md:inline text-slate-700 hover:underline">About</li></Link>
+            <Link href={'/sign-in'}><li className="hidden md:inline text-slate-700 hover:underline">Sign In</li></Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
